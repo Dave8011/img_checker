38631fc18b18f6b474a910a29da9c0e0c82765fe
Dave8011 <saurabhdave8@gmail.com>
2026-01-14 12:07:18 +0530

    Delete check_json.js

--- check_json.js
+++
@@ -1,9 +0,0 @@
-
-const fs = require('fs');
-try {
-    const data = fs.readFileSync('product-gallery/products.json', 'utf8');
-    JSON.parse(data);
-    console.log('JSON is valid');
-} catch (e) {
-    console.error('JSON Error:', e.message);
-}
